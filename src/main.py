4308ffc2049f369986f06cb035c3b7ffd32e3fc7
Jesus Torres <j_torres24@u.pacific.edu>
2024-06-10 14:53:48 -0700

    clean up +  testing querry

--- src/main.py
+++ src/main.py
@@ -1,16 +1,32 @@
 import os
 import pypyodbc as podbc   
 import pandas as pd 
+import numpy as np 
 import warnings
+
 warnings.filterwarnings('ignore')
 
 # Connecting to local server.
-con = podbc.connect("Driver={ODBC Driver 18 for SQL Server};"
-                    "SERVER=THE-FATHER\SQLEXPRESS;"
-                    "DATABASE=MMA-Stats;"
-                    "Trusted_Connection=yes;"
-                    "TrustServerCertificate=yes")
+DRIVER_NAME = 'ODBC Driver 18 for SQL Server'
+SERVER_NAME = 'THE-FATHER\SQLEXPRESS'
+DATABASE_NAME = 'MMA-Stats'
+connection_string =f"""
+            Driver={{{DRIVER_NAME}}};
+            SERVER={SERVER_NAME};
+            DATABASE={DATABASE_NAME};
+            Trusted_Connection=yes;
+            TrustServerCertificate=yes;"""
+connection = podbc.connect(connection_string)
 
-SQL_Query = pd.read_sql_query('''SELECT * FROM [dbo].[ufc-fighters-statistics]''', con)
+cursor = connection.cursor() 
+cursor.execute('SELECT TOP 3 * FROM [dbo].[ufc-fighters-statistics]')
+for row in cursor.fetchall():
+    print(row)
 
-SQL_Query.head()
+print(connection)
+
+
+# SQL_Query = pd.read_sql_query('''SELECT TOP 3 * FROM [dbo].[ufc-fighters-statistics]''', connection)
+
+
+# SQL_Query.head()
