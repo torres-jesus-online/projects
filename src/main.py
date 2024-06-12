db6a62d0da6a631ec14c6239808d80376f5f6d1c
Jesus Torres <j_torres24@u.pacific.edu>
2024-06-11 18:56:28 -0700

    class creation + display fix

--- src/main.py
+++ src/main.py
@@ -3,30 +3,29 @@ import pypyodbc as podbc
 import pandas as pd 
 import numpy as np 
 import warnings
+from IPython.display import display
 
 warnings.filterwarnings('ignore')
-
-# Connecting to local server.
-DRIVER_NAME = 'ODBC Driver 18 for SQL Server'
-SERVER_NAME = 'THE-FATHER\SQLEXPRESS'
-DATABASE_NAME = 'MMA-Stats'
-connection_string =f"""
-            Driver={{{DRIVER_NAME}}};
-            SERVER={SERVER_NAME};
-            DATABASE={DATABASE_NAME};
-            Trusted_Connection=yes;
-            TrustServerCertificate=yes;"""
-connection = podbc.connect(connection_string)
-
-cursor = connection.cursor() 
-cursor.execute('SELECT TOP 3 * FROM [dbo].[ufc-fighters-statistics]')
-for row in cursor.fetchall():
-    print(row)
-
-print(connection)
+def main():
+    # Connecting to local server.
+    DRIVER_NAME = 'ODBC Driver 18 for SQL Server'
+    SERVER_NAME = 'THE-FATHER\SQLEXPRESS'
+    DATABASE_NAME = 'MMA-Stats'
+    connection_string =f"""
+                Driver={{{DRIVER_NAME}}};
+                SERVER={SERVER_NAME};
+                DATABASE={DATABASE_NAME};
+                Trusted_Connection=yes;
+                TrustServerCertificate=yes;"""
+    connection = podbc.connect(connection_string)
 
 
-# SQL_Query = pd.read_sql_query('''SELECT TOP 3 * FROM [dbo].[ufc-fighters-statistics]''', connection)
+    SQL_Query = pd.read_sql_query('''SELECT TOP 3 * FROM [dbo].[ufc-fighters-statistics]''', connection)
+    SQL_Query.head()
 
+    display(SQL_Query)
 
-# SQL_Query.head()
+    connection.close()
+
+if __name__ == "__main__":
+    main()
